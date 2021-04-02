c44d62726e020f568996a5c2681cea6714b27ea5
Lucas Basso <lucas.rbasso@outlook.com>
2021-04-02 15:48:29 -0300

    controllers: orders controllers create

--- src/modules/orders/infra/http/controller/OrdersController.ts
+++ src/modules/orders/infra/http/controller/OrdersController.ts
@@ -7,10 +7,33 @@ import FindOrderService from '@modules/orders/services/FindOrderService';
 
 export default class OrdersController {
   public async show(request: Request, response: Response): Promise<Response> {
-    // TODO
+    try {
+      const { id } = request.params;
+
+      const findOrderService = container.resolve(FindOrderService);
+
+      const order = await findOrderService.execute({ id });
+
+      return response.json(order);
+    } catch (err) {
+      return response.status(400).json({ error: err });
+    }
   }
 
   public async create(request: Request, response: Response): Promise<Response> {
-    // TODO
+    try {
+      const { customer_id, products } = request.body;
+
+      const createOrderService = container.resolve(CreateOrderService);
+
+      const order = await createOrderService.execute({
+        customer_id,
+        products,
+      });
+
+      return response.json(order);
+    } catch (err) {
+      return response.status(400).json({ error: err });
+    }
   }
 }
